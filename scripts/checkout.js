2306bd81e36088a0af3cc571ed87ebf91f0774d4
Sharique Ahmad <ahmedsharique250@gmail.com>
2025-03-04 00:18:57 +0530

    delete cart item

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,7 @@ products.forEach((product)=>{
 });
 
 cartSummaryHTML+=  `
-     <div class="cart-item-container">
+     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -98,7 +98,11 @@ document.querySelectorAll('.js-delete-link').forEach( (link)=>{
   link.addEventListener('click',()=>{
    const productID = link.dataset.productId;
    removeFromCart(productID);
+
+   const container = document.querySelector(`.js-cart-item-container-${productID}`);
+   container.remove();
   
   });
+  
 
 });
